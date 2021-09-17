ff5c3cf30cb16e4c43f8803a9818179344b9a207
unknown <devchrisdai@outlook.com>
2021-09-17 16:48:11 +0800

    change node modules

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,28 +1,63 @@
-
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.course}</h1>
+    </div>
+  );
+};
+const Content = (props) => {
+  return (
+    <div>
+      <p>
+        {props.parts[0].name}
+        {props.parts[0].exercises}
+      </p>
+      <p>
+        {props.parts[1].name}
+        {props.parts[1].exercises}
+      </p>
+      <p>
+        {props.parts[2].name}
+        {props.parts[2].exercises}
+      </p>
+    </div>
+  );
+};
+const Total = (props) => {
+  return (
+    <div>
+      <p>
+        Number of exercises{" "}
+        {props.parts[0].exercises +
+          props.parts[1].exercises +
+          props.parts[2].exercises}
+      </p>
+    </div>
+  );
+};
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = "Half Stack application development";
+
+  const parts = [
+    { name: "Fundamentals of React", exercises: 10 },
+
+    {
+      name: "Using props to pass data",
+      exercises: 7,
+    },
+    {
+      name: "State of a component",
+      exercises: 14,
+    },
+  ];
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course={course} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
