789e2f9eb6726a2a4863f003d0ea9fcb9a0be3aa
unknown <devchrisdai@outlook.com>
2021-09-22 18:30:40 +0800

    1.11

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -5,9 +5,14 @@ const Button = (props) => {
 };
 const StatisticLine = ({ text, value }) => {
   return (
-    <p>
-      {text} {value}
-    </p>
+    <table>
+      <tbody>
+        <tr>
+          <td>{text}</td>
+          <td>{value}</td>
+        </tr>
+      </tbody>
+    </table>
   );
 };
 const Statistics = (props) => {
