29d9b84edfc400e0b172b0038326833c7ee1f789
Chris Dai <devchrisdai@outlook.com>
2021-09-29 13:50:11 +0800

    1.13

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -10,12 +10,22 @@ const App = () => {
     "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
     "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients",
   ];
-
+  const [points, setPoints] = useState(Array(7).fill(0));
   const [selected, setSelected] = useState(0);
-
+  const copy = [...points];
   return (
     <div>
       <p>{anecdotes[selected]}</p>
+      <p>has {points[selected]} votes</p>
+      <button
+        onClick={() => {
+          copy[selected] += 1;
+          setPoints(copy);
+          console.log(points);
+        }}
+      >
+        vote
+      </button>
       <button
         onClick={() => {
           setSelected(Math.floor(Math.random() * 7));
