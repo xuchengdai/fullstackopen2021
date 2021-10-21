b44be6df607f956772030fe247094349701e81cd
Chris Dai <chrisdai@Chriss-MacBook-Air.local>
2021-10-21 13:28:23 +0800

    2.8

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -3,15 +3,20 @@ import Person from "./components/person";
 const App = () => {
   const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
 
   const handleNameChange = (event) => {
     setNewName(event.target.value);
   };
-  const addName = (event) => {
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value);
+  };
+  const addPerson = (event) => {
     event.preventDefault();
 
     const personObject = {
       name: newName,
+      number: newNumber,
     };
 
     persons.find((person) => person.name === newName)
@@ -24,9 +29,10 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addName}>
+      <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
+          number: <input value={newNumber} onChange={handleNumberChange} />
         </div>
         <div>
           <button type="submit">add</button>
--- part2/phonebook/src/components/person.js
+++ part2/phonebook/src/components/person.js
@@ -1,5 +1,9 @@
 import React from "react";
 const Person = (props) => {
-  return <p>{props.person.name}</p>;
+  return (
+    <p>
+      {props.person.name} {props.person.number}
+    </p>
+  );
 };
 export default Person;
